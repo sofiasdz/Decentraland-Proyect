96e2be16591a9f8c485ad7d9a0dd289d8e0f7d15
dimi <stefangeorges4799@gmail.com>
2024-11-13 10:33:58 -0300

    .

--- wsdl2java/org/datacontract/schemas/_2004/_07/ua_maestrodepacientes_business_modules/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.datacontract.org/2004/07/UA.MaestroDePacientes.Business.Modules.PacientesService", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.datacontract.schemas._2004._07.ua_maestrodepacientes_business_modules;
